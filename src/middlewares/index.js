c8a6ed2e0a11f36a9ec54575eea5827f96caf0a2
Yasmin silva <yasmiinssilva26@gmail.com>
2023-11-26 21:59:37 -0300

    :sparkles: creation of the client middlewares and user repository

---
+++ src/middlewares/index.js
@@ -0,0 +1,7 @@
+const verifyClientExists = require("./verifyClientExists");
+const verifyIfIdValid = require("./verifyIfIdValid");
+
+module.exports = {
+    verifyClientExists,
+    verifyIfIdValid
+}
