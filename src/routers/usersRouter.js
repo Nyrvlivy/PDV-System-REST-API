dde5865f885bb66c99239778de938a3b5da21d14
Nyrvlivy <nyrvlivy@gmail.com>
2023-11-27 21:50:44 -0300

    :scroll: Add Swagger documentation for API endpoints

--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -1,3 +1,44 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Usuários
+ *   description: Operações relacionadas a usuários
+ */
+
+/**
+ * @swagger
+ * /usuario:
+ *   get:
+ *     summary: Obtém o perfil do usuário
+ *     description: Retorna as informações do perfil do usuário autenticado
+ *     tags: [Usuários]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Sucesso
+ *         content:
+ *           application/json:
+ *             example:
+ *               id: 1
+ *               nome: "Nome do Usuário"
+ *               email: "usuario@example.com"
+ *       401:
+ *         description: Não autorizado
+ *       404:
+ *         description: Usuário não encontrado
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: "Cliente não encontrado."
+ *       500:
+ *         description: Erro interno do servidor
+ *         content:
+ *           application/json:
+ *             example:
+ *               mensagem: "Erro interno do servidor!"
+ */
+
 const { Router } = require("express");
 const usersRouter = Router();
 const { getUserProfileController } = require("../controllers");
