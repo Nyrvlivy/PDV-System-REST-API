35d2b4ef6dfb5b4eb0ef25359babc7544fd68666
Nyrvlivy <nyrvlivy@gmail.com>
2023-11-29 21:34:16 -0300

    :memo: Update Swagger documentation

--- docs/swagger.js
+++ docs/swagger.js
@@ -1,7 +1,6 @@
 const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const path = require("path");
-const { userSchema } = require("../src/schemas");
 
 const options = {
     definition: {
