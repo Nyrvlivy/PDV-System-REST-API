6b82023f71d842168b394d0528601089eed123c0
Nyrvlivy <nyrvlivy@gmail.com>
2023-12-03 17:24:41 -0300

    :sparkles: Add delete product function and :zap: refactor code

---
+++ src/controllers/products/deleteProductController.js
@@ -0,0 +1,31 @@
+const { deleteProductService } = require("../../services");
+const {
+    MissingParamError,
+    InvalidParamError,
+    NotFoundError,
+} = require("../../errors");
+
+const deleteProductController = {
+    async handle(req, res) {
+        try {
+            const { id } = req.params;
+            await deleteProductService.execute(id);
+
+            res.status(204).end();
+        } catch (error) {
+            console.log(error);
+            if (
+                error instanceof MissingParamError ||
+                error instanceof InvalidParamError
+            ) {
+                return res.status(400).json({ error: error.message });
+            }
+            if (error instanceof NotFoundError) {
+                return res.status(404).json({ error: error.message });
+            }
+            res.status(500).json({ error: "Erro interno no servidor." });
+        }
+    },
+};
+
+module.exports = deleteProductController;
