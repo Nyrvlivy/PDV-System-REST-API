8899b06329a297c38019f017e8345b4c544b1d37
Nyrvlivy <nyrvlivy@gmail.com>
2023-12-12 17:46:47 -0300

    :zap: Refactor createOrder function

--- src/repositories/ordersRepository.js
+++ src/repositories/ordersRepository.js
@@ -1,6 +1,38 @@
 const connection = require("../configs/database/connection/connection");
 
 const ordersRepository = {
+    create: async function (orderData, productsData) {
+        let order;
+        let savedProducts = [];
+
+        await connection.transaction(async function (trx) {
+            try {
+                order = await connection("pedidos")
+                    .returning("*")
+                    .insert(orderData)
+                    .transacting(trx);
+
+                for (const productData of productsData) {
+                    const [savedProduct] = await connection("pedido_produtos")
+                        .returning("*")
+                        .insert({
+                            pedido_id: order[0].id,
+                            ...productData,
+                        })
+                        .transacting(trx);
+                    savedProducts.push(savedProduct);
+                }
+
+                await trx.commit();
+            } catch (error) {
+                await trx.rollback();
+                throw error;
+            }
+        });
+
+        return { pedidos: order[0], pedido_produtos: savedProducts };
+    },
+
     getAll: async function () {
         const orders = await connection("pedidos").select("*");
         return orders;
@@ -10,6 +42,13 @@ const ordersRepository = {
             .select("*")
             .where({ cliente_id });
         return orders;
+    },
+    getById: async function (pedido_id) {
+        const order = await connection("pedidos")
+            .select("*")
+            .where({ pedido_id })
+            .first();
+        return order;
     },
 };
 
--- src/services/orders/createOrderService.js
+++ src/services/orders/createOrderService.js
@@ -1,84 +1,64 @@
-const connection = require("../../configs/database/connection/connection");
-const { validateOrderCreate, verifyCustomerExists } = require("../../utils");
+const {
+    validateOrderCreate,
+    verifyProductExists,
+    verifyCustomerExists,
+} = require("../../utils");
 const {
     productsRepository,
     customersRepository,
+    ordersRepository,
 } = require("../../repositories");
-const { NotFoundError } = require("../../errors");
+const { InvalidParamError } = require("../../errors");
 const { OrderEmailService } = require("../email");
 
 const createOrderService = {
     async execute(payload) {
-        validateOrderCreate(payload);
         const { cliente_id, observacao, pedido_produtos } = payload;
+        validateOrderCreate(payload);
         await verifyCustomerExists(cliente_id);
 
-        let orderProductsData;
-        await connection.transaction(async function (trx) {
-            try {
-                let valor_total = 0;
-                const savedProducts = [];
-                let [order] = await connection("pedidos")
-                    .returning("*")
-                    .insert({
-                        cliente_id,
-                        observacao,
-                        valor_total: 0,
-                    })
-                    .transacting(trx);
-                for (const orderProducts of pedido_produtos) {
-                    const product = await productsRepository.getByPk(
-                        orderProducts.produto_id
-                    );
-                    if (!product)
-                        throw new NotFoundError("Produto não encontrado.");
+        const orderData = {
+            cliente_id,
+            observacao,
+            valor_total: 0,
+        };
 
-                    if (
-                        product.quantidade_estoque <
-                        orderProducts.quantidade_produto
-                    ) {
-                        throw new NotFoundError(
-                            "Quantidade em estoque insuficiente."
-                        );
-                    }
+        const productsData = [];
 
-                    valor_total +=
-                        product.valor * orderProducts.quantidade_produto;
-                    const [savedProduct] = await connection("pedido_produtos")
-                        .returning("*")
-                        .insert({
-                            pedido_id: order.id,
-                            produto_id: product.id,
-                            quantidade_produto:
-                                orderProducts.quantidade_produto,
-                            valor_produto: product.valor,
-                        })
-                        .transacting(trx);
-                    savedProducts.push(savedProduct);
-                }
-                order = await connection("pedidos")
-                    .returning("*")
-                    .where("id", order.id)
-                    .update({ valor_total })
-                    .transacting(trx);
+        for (const orderProducts of pedido_produtos) {
+            const product = await productsRepository.getByPk(
+                orderProducts.produto_id
+            );
 
-                await trx.commit();
+            await verifyProductExists(orderProducts.produto_id);
 
-                orderProductsData = {
-                    pedido: order[0],
-                    pedido_produtos: savedProducts,
-                };
-
-                const customerOrderEmail =
-                    await customersRepository.getEmailByPk(cliente_id);
-
-                await OrderEmailService.sendOrderConfirmationEmail(
-                    customerOrderEmail
+            if (product.quantidade_estoque < orderProducts.quantidade_produto) {
+                throw new InvalidParamError(
+                    "Quantidade em estoque insuficiente."
                 );
-            } catch (error) {
-                throw error;
             }
-        });
+
+            orderData.valor_total +=
+                product.valor * orderProducts.quantidade_produto;
+
+            productsData.push({
+                produto_id: product.id,
+                quantidade_produto: orderProducts.quantidade_produto,
+                valor_produto: product.valor,
+            });
+        }
+
+        const orderProductsData = await ordersRepository.create(
+            orderData,
+            productsData
+        );
+
+        const customerOrderEmail = await customersRepository.getEmailByPk(
+            cliente_id
+        );
+
+        await OrderEmailService.sendOrderConfirmationEmail(customerOrderEmail);
+
         return orderProductsData;
     },
 };
